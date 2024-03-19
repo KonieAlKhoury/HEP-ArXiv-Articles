8a154f4da88282db582cb21bd4283e52f4270281
Konie <konie.al.khoury@gmail.com>
2024-03-19 21:28:18 +0100

    Add arXiv API integration and data retrieval

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
 # HEP-ArXiv-Articles
 
 Another way to import is using arxiv library
-# import arxiv
+```python
+import arxiv
 
-# # Define the search query for the ArXiv API
-# # This query will return the last 100 papers in the 'hep-ph' category
-# query = 'cat:hep-ph AND all:ATLAS'
+#Define the search query for the ArXiv API
+#This query will return the last 100 papers in the 'hep-ph' category
+query = 'cat:hep-ph AND all:ATLAS'
 
 # # Use arxiv to fetch the data
-# search = arxiv.Search(query=query, max_results=100, sort_by=arxiv.SortCriterion.SubmittedDate)
+search = arxiv.Search(query=query, max_results=100, sort_by=arxiv.SortCriterion.SubmittedDate)
 
 # # Print the titles of the papers
-# for result in search.results():
-#     print(result.title)
+for result in search.results():
+    print(result.title)
+```
--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 import feedparser
 
 # Define the URL for the ArXiv API
-def load_data(query , max_results, add_options=''):
- 
+def load_data(query, max_results, add_options=''):
+    """
+    Loads data from the arXiv API based on the given query.
+
+    Args:
+        query (str): The search query to be used.
+        max_results (int): The maximum number of results to retrieve.
+        add_options (str, optional): Additional options to be added to the API query. Defaults to ''.
+
+    Returns:
+        dict: A dictionary containing the parsed data from the arXiv API.
+
+    Raises:
+        None
+
+    """
     url = f'http://export.arxiv.org/api/query?search_query={query}&start=0&max_results={max_results}{add_options}'
 
     data = feedparser.parse(url)
@@ -17,19 +31,22 @@ def load_data(query , max_results, add_options=''):
     return data
 
 
+def main():
+    # This load the data of the last 200 papers in the 'hep-ph' category and from the ATLAS experiment
+    query = 'cat:hep-ph+AND+all:ATLAS'
+    max_results=200
+    add_options = '&sortBy=lastUpdatedDate&sortOrder=descending'
 
-# This load the data of the last 200 papers in the 'hep-ph' category and from the ATLAS experiment
-query = 'cat:hep-ph+AND+all:ATLAS'
-max_results=200
-add_options = '&sortBy=lastUpdatedDate&sortOrder=descending'
+    data = load_data(query , max_results, add_options)
 
-data = load_data(query , max_results, add_options)
-
-# Print the keys of the first entry to see what is in the data
-print(data.entries[0].keys())
+    # Print the keys of the first entry to see what is in the data
+    print(data.entries[0].keys())
 
 
-# for entry in data.entries:
-#     print(entry.title)
-#     print(entry.summary)
-#     print("**")
+    for entry in data.entries:
+        print(entry.title)
+        print(entry.summary)
+        print("**")
+
+if __name__ == '__main__':
+    main()
