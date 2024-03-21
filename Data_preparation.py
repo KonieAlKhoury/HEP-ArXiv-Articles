45e91b92f010e79ea7f61cece1e72becf70c8abd
Konie <konie.al.khoury@gmail.com>
2024-03-21 22:36:01 +0100

    Update the new table on PostgreSQL

--- Data_preparation.py
+++ Data_preparation.py
@@ -1,11 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy import text
-import psycopg2
 import pandas as pd
+import nltk
+from nltk.corpus import stopwords
+nltk.download('stopwords')
 
-def main():
+def Get_data_postgres(database,table):
     # define the postgres connection string
-    connection_str = "postgresql://postgres:POSTgres21032024@localhost:5432/Arxiv_data"
+    connection_str = f"postgresql://postgres:POSTgres21032024@localhost:5432/{database}"
 
     #Create a SQLAlchemy engine
     engine = create_engine(connection_str)
@@ -17,15 +19,52 @@ def main():
         print(f'Sorry failed to connect: {ex}')
 
  
-    query = 'SELECT * FROM public."Last_200_papers"'
+    query = f'SELECT * FROM public."{table}"'
     with engine.connect() as connection:
         results = connection.execute(text(query)).fetchall()
-        
+
     # Convert the results to a pandas DataFrame
     df = pd.DataFrame(results)
-    #Check the first row
-    print(df.iloc[0])
 
+    return df
+
+def main():
+    #Check the first row
+    database = "Arxiv_data"
+    table = "Last_200_papers"
+    df_Arxiv = Get_data_postgres(database,table)
+
+    # start preparing the data by setting the title and suummary to lower case
+    df_Arxiv['title'] = df_Arxiv['title'].str.lower()
+    df_Arxiv['summary'] = df_Arxiv['summary'].str.lower()
+    
+    # remove "\n" from title and summary and convert them to a space
+    df_Arxiv['title'] = df_Arxiv['title'].str.replace("\n", " ")
+    df_Arxiv['summary'] = df_Arxiv['summary'].str.replace("\n", " ")
+
+    #remove the stop/filling words, such as the, I , we , and, etc .. 
+    # Using the stopwords from the nltk package and setting it to english
+    stops = stopwords.words('english')
+    # Split the title and summary into words
+    df_Arxiv['title'] = df_Arxiv['title'].str.split()
+    df_Arxiv['summary'] = df_Arxiv['summary'].str.split()
+    # Remove the stop words
+    df_Arxiv['title'] = df_Arxiv['title'].apply(lambda x: [item for item in x if item not in stops])
+    df_Arxiv['summary'] = df_Arxiv['summary'].apply(lambda x: [item for item in x if item not in stops])
+
+    #remove all numbers from the title and summary
+    df_Arxiv['title'] = df_Arxiv['title'].apply(lambda x: [item for item in x if item.isdigit() == False])
+    df_Arxiv['summary'] = df_Arxiv['summary'].apply(lambda x: [item for item in x if item.isdigit() == False])
+
+
+    #Pusing the table to a new table in the same database with the name test1
+    new_table = "Last_200_papers_processed"   
+    connection_str = f"postgresql://postgres:POSTgres21032024@localhost:5432/{database}"
+
+    #Create a SQLAlchemy engine
+    engine = create_engine(connection_str)
+    #Push the data to the new table, if exists append the data
+    df_Arxiv.to_sql(new_table, con=engine, if_exists='append',index=False)
 
 
 if __name__ == "__main__":
